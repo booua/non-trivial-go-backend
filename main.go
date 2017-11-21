6c44ced1877f629ae909a691098426c4d00b2cdd
Andrzej Wiącek <wiacek.and@gmail.com>
2017-11-21 18:54:30 +0100

    Create get returning opinions in JSON format

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 func main() {
     router := mux.NewRouter()
     router.HandleFunc("/test", TestRouting).Methods("GET")
-    fmt.Println("Server started")
+    router.HandleFunc("/opinions", opinions).Methods("GET")
+    fmt.Println("Server started on http://localhost:8000")
     log.Fatal(http.ListenAndServe(":8000", router))
 }
 
@@ -21,4 +22,23 @@ func TestRouting(w http.ResponseWriter, r *http.Request) {
 
     testArray := [10]string{"TEST1","TEST2","TEST3","TEST4","TEST5","TEST6","TEST7","TEST8","TEST9","TEST10"}
     json.NewEncoder(w).Encode(testArray)
+}
+
+func opinions(w http.ResponseWriter, r *http.Request) {
+
+  type opinion struct {
+    Author string `json:"author"`
+    Content string `json:"content"`
+    Rating int `json:"rating"`
+  }
+
+  var opinionsList = []opinion{
+    {"Sąsiad Ziutka", "Ziutek kupił Grażynie książkę zamiast sobie kratę browarów. Żałuje. A ja nie, bo i tak stukam jego żonę.", 10},
+    {"Trzyletni Jaś", "Ghyy ghyy bybyyy ghuuuu", 3},
+    {"Samotna matka", "Mój syn nie docenił prezentu. Może dlatego, że jest niewidomy.", 5},
+    {"Grafik", "Kto projektował tę stronę? Ja pierdziu, nie mogę na to patrzeć.", 0},
+  }
+
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+  json.NewEncoder(w).Encode(opinionsList)
 }
