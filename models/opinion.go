ec04172c574c94d04a264e5f5e1da94ce41cc5b6
Marcin Szewczyk <marcin91.szewczyk@gmail.com>
2017-11-25 14:07:20 +0100

    Change model to fit the requirements of React frontend

--- models/opinion.go
+++ models/opinion.go
@@ -5,6 +5,6 @@ import "gopkg.in/mgo.v2/bson"
 type Opinion struct {
 	Id     bson.ObjectId `json:"id" bson:"_id"`
 	Author string        `json:"author" bson:"author"`
-	Text   string        `json:"text" bson:"text"`
+	Text   string        `json:"content" bson:"text"`
 	Rating int           `json:"rating" bson:"rating"`
 }
